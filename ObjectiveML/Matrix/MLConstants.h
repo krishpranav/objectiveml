fc43706fe8a7a0d6735cf54fa66c2b629861cdbb
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-03 16:19:46 +0530

    ObjectiveML: Matrix[MLConstants]

---
+++ ObjectiveML/Matrix/MLConstants.h
@@ -0,0 +1,16 @@
+/**
+ * @file MLConstants.h
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-03
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+#ifndef MLConstants_h
+#define MLConstants_h
+
+#define ARC4RANDOM_MAX      0x100000000
+
+#endif /* MLConstants_h */
