0759efdb98dc33bcab04c229a8a7a5ea4609f6f8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-03 16:07:52 +0530

    ObjectiveML: Tensor[MLTensor]

---
+++ ObjectiveML/Tensor/MLTensor.h
@@ -0,0 +1,12 @@
+//
+//  MLTensor.h
+//  ObjectiveML
+//
+//  Created by Krisna Pranav on 03/05/25.
+//
+
+#ifndef MLTensor_h
+#define MLTensor_h
+
+
+#endif /* MLTensor_h */
---
+++ ObjectiveML/Tensor/MLTensor.m
@@ -0,0 +1,8 @@
+//
+//  MLTensor.m
+//  ObjectiveML
+//
+//  Created by Krisna Pranav on 03/05/25.
+//
+
+#import <Foundation/Foundation.h>
