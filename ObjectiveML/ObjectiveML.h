ac7d03096f0e6a480c836eefe857e3e47f02461a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-03 16:06:41 +0530

    add files via upload

---
+++ ObjectiveML/ObjectiveML.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveML.h
+//  ObjectiveML
+//
+//  Created by Krisna Pranav on 03/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveML.
+FOUNDATION_EXPORT double ObjectiveMLVersionNumber;
+
+//! Project version string for ObjectiveML.
+FOUNDATION_EXPORT const unsigned char ObjectiveMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveML/PublicHeader.h>
+
+
