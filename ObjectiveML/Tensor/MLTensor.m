1c04f608624a293f90ec32cae93e9857adc0ca0a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-03 16:11:19 +0530

    ObjectiveML: Tensor[MLTensor]

--- ObjectiveML/Tensor/MLTensor.m
+++ ObjectiveML/Tensor/MLTensor.m
@@ -1,8 +1,37 @@
-//
-//  MLTensor.m
-//  ObjectiveML
-//
-//  Created by Krisna Pranav on 03/05/25.
-//
+/**
+ * @file MLTensor.m
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-03
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
 
-#import <Foundation/Foundation.h>
+#import "MLTensor.h"
+
+@implementation MLTensor
+
+- (instancetype)initWithRows:(NSInteger)rows cols:(NSInteger)cols {
+    self = [super init];
+
+    if (self) {
+        _rows = rows;
+        _cols = cols;
+        _data = [NSMutableArray arrayWithCapacity:rows];
+
+        for (int i = 0; i < rows; i++) {
+            NSMutableArray *row = [NSMutableArray arrayWithCapacity:cols];
+
+            for (int j = 0; j < cols; j++) {
+                [row addObject:@0.0];
+            }
+
+            [_data addObject:row];
+        }
+    }
+
+    return self;
+}
+
+@end
