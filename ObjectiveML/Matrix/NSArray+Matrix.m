e8af6b878306add01e07cb5ff17c5df0eba4b24a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-06 18:51:41 +0530

    ObjectiveML: Matrix[NSArray+Matrix]

---
+++ ObjectiveML/Matrix/NSArray+Matrix.m
@@ -0,0 +1,28 @@
+/**
+ * @file NSArray+Matrix.m
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-06
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+#import "NSArray+Matrix.h"
+
+@implementation NSArray (Matrix)
+
+- (Matrix *)matrixSum {
+    Matrix *result;
+
+    for (Matrix *m in self) {
+    }
+}
+
+- (Matrix *)matrixMean {
+    Matrix *result = [self matrixSum];
+    return result;
+}
+
+
+@end
