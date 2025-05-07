68e90dc209a36331db5bbec31d4f40e5471c815f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-07 17:55:43 +0530

    ObjectiveML: DataFrame[NSIndexSet+Sampling]

---
+++ ObjectiveML/DataFrame/NSIndexSet+Sampling.m
@@ -0,0 +1,25 @@
+/**
+ * @file NSIndexSet+Sampling.m
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-07
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+#import "NSIndexSet+Sampling.h"
+
+#define ARC4RANDOM_MAX 0x1000
+
+@implementation NSIndexSet (Sampling)
+
++ (instancetype)indexesForSampling:(NSUInteger)samples inRange:(NSRange)range replacement:(BOOL)replacement {
+    if (replacement) {
+
+    } else {
+        
+    }
+}
+
+@end
