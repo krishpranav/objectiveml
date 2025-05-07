4da97a88511214a97191ec492d290a4713f8833a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-07 17:28:09 +0530

    ObjectiveML: DataFrame[MLMissingValue]

---
+++ ObjectiveML/DataFrame/MLMissingValue.h
@@ -0,0 +1,17 @@
+/**
+ * @file MLMissingValue.h
+ * @author Krisna Pranav
+ * @version 1.0
+ * @date 2025-05-03
+ *
+ * @copyright Copyright (c) 2025 Krisna Pranav
+ *
+ */
+
+@import Foundation;
+
+@interface MLMissingValue : NSObject <NSCoding, NSCopying>
+
++ (instancetype)missingValue;
+
+@end
