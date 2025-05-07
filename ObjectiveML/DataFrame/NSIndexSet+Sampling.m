0719a5eb56daf5abfa35efc00351fb2c4af0bd8a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-05-07 17:56:17 +0530

    ObjectiveML: DataFrame[NSIndexSet+Sampling]

--- ObjectiveML/DataFrame/NSIndexSet+Sampling.m
+++ ObjectiveML/DataFrame/NSIndexSet+Sampling.m
@@ -10,16 +10,35 @@
 
 #import "NSIndexSet+Sampling.h"
 
-#define ARC4RANDOM_MAX 0x1000
+#define ARC4RANDOM_MAX      0x100000000
 
 @implementation NSIndexSet (Sampling)
 
-+ (instancetype)indexesForSampling:(NSUInteger)samples inRange:(NSRange)range replacement:(BOOL)replacement {
-    if (replacement) {
++ (instancetype)indexesForSampling:(NSUInteger)samples inRange:(NSRange)range replacement:(BOOL)replacement
+{
+    NSMutableIndexSet *selectedIndexes = [NSMutableIndexSet indexSet];
 
+    if (replacement) {
+        NSUInteger N = range.length;
+        for (NSUInteger i=0; i<samples; i++) {
+            [selectedIndexes addIndex:range.location + (int)(N * (double)arc4random() / ARC4RANDOM_MAX)];
+        }
     } else {
-        
+        NSUInteger N = range.length;
+        NSUInteger i = 0;
+        NSUInteger n = MIN(N, samples);
+
+        while (n > 0) {
+            if (N * (double)arc4random() / ARC4RANDOM_MAX <= n) {
+                [selectedIndexes addIndex:range.location + i];
+                n--;
+            }
+
+            i++;
+            N--;
+        }
     }
+    return selectedIndexes;
 }
 
 @end
